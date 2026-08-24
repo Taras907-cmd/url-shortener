947f9f42feeb0a9f48955805b66c5c47b7bccee1
Taras907-cmd <mainbox84@ukr.net>
2026-08-24 16:06:35 +0300

    2

--- demo/src/main/java/com/example/demo/service/NoteServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/NoteServiceImpl.java
@@ -1,5 +1,7 @@
-package com.example.demo;
+package com.example.demo.service;
 
+import com.example.demo.model.Note;
+import com.example.demo.repository.NoteFakeResponsitory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
